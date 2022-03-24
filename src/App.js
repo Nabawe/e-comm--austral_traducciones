f0385d150633d29f7bdb43827321d46166a735eb
Nabawe <Nabawe@users.noreply.github.com>
2022-03-24 14:04:12 -0300

    Recordar SIEMPRE hacer un commit antes de una branch x2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-          "Hello World"
-      </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>
+                "Hello World"
+            </p>
+        </div>
+    );
 }
 
 export default App;
